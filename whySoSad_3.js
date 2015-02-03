7060dc14deff0ee500956b7baf040892b45b0cf7
jonathangravato <jonathan@gravatodesign.com>
2015-02-02 19:02:02 -0500

    Added flickr and feelz
    Have it retweeting but not posting reply with img link

--- whySoSad_3.js
+++ whySoSad_3.js
@@ -6,8 +6,134 @@ var T = new Twit({
   , access_token:         '2989837761-HvgQxbXSrBVgxgyBI1YYzjwU7AaJ3U2xFPGQlIM'
   , access_token_secret:  'TZSi4HDBdOGBtslAmHLoPDNSMHtiHsmgi5tvcY2EA0E6G'
 });
+var Flickr = require('flickr').Flickr;
+var client = new Flickr('ae6667eb03bf378f6a47dfd80d02352d', '733a34feb0798834', 
+                        {"oauth_token": 'optional oauth token', "oauth_token_secret": 'optional oauth token secret'});
 var tweet_count = 0;
 var prev_sadness = [];
+var message = '';
+var prev_message = 0;
+
+function newMessage (prev_message) {
+	var message_num = Math.floor((Math.random() * 10) + 1);
+	if (message_num != prev_message) {
+		switch(message_num) {
+    		case 1:
+        		message = "Feeling down? Here's a kitten. ";
+        		prev_message = 1;
+        		break;
+    		case 2:
+        		message = "If you having girl problems, I feel bad for you son...Here's a kitten. ";
+		        prev_message = 2;
+		        break;
+		    case 3:
+		        message = "Wah wah, boo hoo? KITTEN! ";
+		        prev_message = 3;
+		        break;
+		    case 4:
+		        message = "We all have bad days.  Here's a little ball of fuzz to turn yours around. ";
+		        prev_message = 4;
+		        break;
+		    case 5:
+		        message = "Need a smile?  Look at this for a while. ";
+		        prev_message = 5;
+		        break;
+		    case 6:
+		        message = "Being sad is fine, but don't stay that way.  Kitten up. ";
+		        prev_message = 6;
+		        break;
+		    case 7:
+		        message = "I know what makes me feel better when I'm down.  Allow me to share. ";
+		        prev_message = 7;
+		        break;
+		    case 8:
+		        message = "Boo Hoo!  I'm sad...Kitten! ";
+		        prev_message = 8;
+		        break;
+		    case 9:
+		        message = "Here's a little furry magic to help you through your bad day. ";
+		        prev_message = 9;
+		        break;
+		    case 10:
+		        message = "It could always be worse.  At least you have this to cheer you up. ";
+		        prev_message = 10;
+		        break;   
+		    default:
+		        "Having a bad day?  Here's a kitten to cheer you up! ";
+		}
+	} else {
+		console.log("Duplicate message selected!\n");
+		message_num = Math.floor((Math.random() * 10) + 1);
+	}
+
+	return message;
+}
+
+var post_tweet = function(message,tweet_handle,imageURL){
+	var cheer_up = "@" + tweet_handle + " " + message + imageURL; 
+	T.post("statuses/update", { status: cheer_up }, function(err, data, response) {
+		console.log("Yes!  We brightened " + tweet_handle + "'s day!");
+	})
+	//console.log("Here's the feelz! " + cheer_up + "\n");
+}
+
+// This function translates a numerical photo ID into a unique base 58 id used in Flickr's url shortening service.
+// Taken from https://www.flickr.com/groups/51035612836@N01/discuss/72157616713786392/72157620673064673
+function translateToBase58 (num){
+    if(typeof num!=='number')
+		num=parseInt(num);
+    //base 58 only has these characters:
+    var enc='', alpha='123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'; 
+    var div=num,mod;
+    // Divide by 58 until we get to 58.
+    while(num>=58){
+	div=num/58; // Divide the number for each digit in base 58
+	mod=num-(58*Math.floor(div)); // select the (base 58) number's alpha-numerical representation
+	enc=''+alpha.substr(mod,1) +enc; // Use substr to get the char from the list
+	num=Math.floor(div); // We don't care about fractions, so apply floor just in case.
+    } 
+    // return either the empty string or the encoded string.
+    return(div)?''+alpha.substr(div,1)+enc:enc;
+}
+
+function getFlickrURL(search_term,tweet_handle) {
+	var flickr_params = {
+		text: search_term,
+		media: "photos",
+		license: "1,2,3,4",
+		safe_search:1,
+		per_page: 25,
+		page: 1,
+		extras: "url_q, url_z, url_b, owner_name"
+	};
+	 client.executeAPIRequest("flickr.photos.search", flickr_params, false, function(err, result) {
+		// Show the error if we got one
+		if(err) {
+			console.log("FLICKR ERROR: " + err);
+			return;
+		}
+		else console.log("Success!");
+
+		var photo1 = result.photos.photo[0];
+		//var http = require('http-request');
+		var imageURL = "http://flic.kr/p/"+translateToBase58(photo1.id);
+		console.log(imageURL);
+		T.post('statuses/update', { status: "@" + tweet_handle + " " + message + imageURL}, function(err, data, response) {
+			if(err)
+				console.log("err: " + err);
+			else
+				console.log("Posted: " + data);
+		 });
+	 	// post_tweet(message,tweet_handle,imageURL);
+	 //return imageURL;
+	});
+}
+
+/*
+
+Start Stream!
+
+*/
 
 var sad_stream = T.stream('statuses/filter', { track: ':(', language: 'en' });
 
@@ -21,9 +147,9 @@ sad_stream.on('tweet', function (tweet) {
 
 		if (tweet_count < 1){
 			var tweet_handle = tweet.user.screen_name;
-			var tweet_id_str = tweet.id_str;
+			var tweet_id = tweet.id_str;
 
-			console.log("Sad Panda: " + tweet_handle + "\n" + "Tweet ID: " + tweet_id_str + "\n" + "Tweet Text: " + tweet.text);
+			console.log("Sad Panda: " + tweet_handle + "\n" + "Tweet ID: " + tweet_id + "\n" + "Tweet Text: " + tweet.text);
 			tweet_count++;
 
 			var has_run = function(){
@@ -39,7 +165,13 @@ sad_stream.on('tweet', function (tweet) {
 
 				sad_stream.stop();
 
-			    T.post('statuses/retweet/:id', { id: tweet_id_str }, 
+				/*
+
+					Retweet!
+
+				*/
+
+			    T.post('statuses/retweet/:id', { id: tweet_id }, 
 			    	function(err, data, res){
 
 					    if(err){
@@ -55,6 +187,28 @@ sad_stream.on('tweet', function (tweet) {
 					});
 
 				prev_sadness.push(tweet_id);
+
+				/*
+
+					Post New!
+
+				*/
+
+				// generate kitten
+
+				getFlickrURL("cute kitten",tweet_handle);  // <---- get imgURL
+
+
+				// generate good feelz
+
+				newMessage(prev_message);
+
+				// put it together and send it
+
+				//post_tweet(message,tweet_handle,imageURL);
+
+				// sad_stream.start();
+
 
 			} else {
 
